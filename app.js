b514cfecc0cf4917a96a2dac17e1ef9aad013037
son4john <49545823+son4john@users.noreply.github.com>
2019-04-14 16:03:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const http = require('http');
 const port=process.env.PORT || 3000
+const fs = require('fs');
+var rawObj = fs.readFileSync('dept_manager.json') //import the raw JSON table
+var parsObj = JSON.parse(rawObj); // parse the data to key value pairs
+
+var stringObj = JSON.stringify(parsObj); // stringify the object
+
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
